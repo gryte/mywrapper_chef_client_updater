eb4ad9bcb60de512fc48874446da0f1d8c2f04b5
Adam Linkous <alinkous@gmail.com>
2018-10-03 19:40:36 -0500

    Update Patch Version
    - Update chef client version attribute to latest
    - Bump metadata patch version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'alinkous+support@gmail.com'
 license 'All Rights Reserved'
 description 'Installs/Configures mywrapper_chef_client_updater'
 long_description 'Installs/Configures mywrapper_chef_client_updater'
-version '0.6.1'
+version '0.6.2'
 supports 'centos'
 chef_version '>= 12.19' if respond_to?(:chef_version)
 issues_url 'https://github.com/gryte/mywrapper_chef_client_updater/issues'
