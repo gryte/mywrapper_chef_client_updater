aab9e2262782756ab81690746b350731cecd1128
Adam Linkous <alinkous@gmail.com>
2019-11-04 17:45:58 -0600

    Push Forward With Issues On Latest Code

--- metadata.rb
+++ metadata.rb
@@ -4,10 +4,10 @@ maintainer_email 'alinkous+support@gmail.com'
 license 'All Rights Reserved'
 description 'Installs/Configures mywrapper_chef_client_updater'
 long_description 'Installs/Configures mywrapper_chef_client_updater'
-version '1.0.6'
+version '1.0.7'
 supports 'centos'
 chef_version '>= 12.19' if respond_to?(:chef_version)
 issues_url 'https://github.com/gryte/mywrapper_chef_client_updater/issues'
 source_url 'https://github.com/gryte/mywrapper_chef_client_updater'
 
-depends 'chef_client_updater', '~> 3.5.2'
+depends 'chef_client_updater', '~> 3.6.0'
